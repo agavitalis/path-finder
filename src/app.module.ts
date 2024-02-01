03a4684cf3b1fa046827c2f77ab0edd44d4ef194
Vitalis Ogbonna <agavitalisogbonna@gmail.com>
2024-02-01 23:29:00 +0100

    configured resturant model entitiy and universal app throttle(rate limitter)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RestaurantModule } from './restaurant/restaurant.module';
-import { ResturantsModule } from './resturants/resturants.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Env } from './config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [RestaurantModule, ResturantsModule, RestaurantsModule],
+  imports: [
+    RestaurantsModule,
+    MongooseModule.forRoot(Env.DB_URL),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, //60 seconds
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,1 +1,36 @@
-export class Restaurant {}
+import { SchemaFactory, Schema, Prop } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type RestaurantDocument = HydratedDocument<Restaurant>;
+
+@Schema({ timestamps: true })
+export class Restaurant {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop({ required: true })
+  location: {
+    type: { type: string; default: 'Point' };
+    coordinates: { type: Array<number> };
+  };
+
+  @Prop({ default: null })
+  cuisines: string[];
+
+  @Prop({ default: 5 })
+  rating: number;
+
+  @Prop({ default: 500 })
+  averageMealPrice: number;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+
+  @Prop({ default: null })
+  deletedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(Restaurant);
